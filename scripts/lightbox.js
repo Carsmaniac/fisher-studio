5fc69c651886ad8a376afe60dde5fa6456f4c6d8
Carsmaniac <mickledapickle@me.com>
2021-10-30 15:45:25 +1100

    Add project page
    A project page has been created with a Vimeo embed and images that open
    in a lightbox built in JavaScript.

---
+++ scripts/lightbox.js
@@ -0,0 +1,22 @@
+function showLightbox(clickedImage) {
+    document.getElementById("lightbox").style.display = "block";
+    document.getElementById("lightbox-image").src = clickedImage.src.replace(".jpg", "-full.jpg");
+}
+
+function hideLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+function maybeHideLightbox() {
+    var image = document.getElementById("lightbox-image");
+    // Only hide the lighbox if the mouse clicked outside the image
+    if (!image.matches(":hover")) {
+        hideLightbox()
+    }
+}
+
+document.addEventListener("keyup", function (keyEvent) {
+    if (event.key == "Escape") {
+        hideLightbox();
+    }
+})
