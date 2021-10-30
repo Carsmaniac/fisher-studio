73a7f391817f4754dccd6fdc125d8b733a9d47cd
Carsmaniac <mickledapickle@me.com>
2021-10-30 21:55:56 +1100

    Add navbar to home page
    A navigation bar has been added to the homepage, placing links to the
    projects, about and contact page up the top, the latter two of which do
    not yet exist. These pages are already accessible via buttons on the
    page, but this makes them more visible and easier to get to.

---
+++ scripts/stick-navbar.js
@@ -0,0 +1,33 @@
+var lastKnownScrollPosition = 0;
+var ticking = false;
+
+const navBar = document.getElementsByTagName("nav")[0]
+
+function doSomething(scrollPos) {
+    var vimeoPos = document.getElementsByClassName("vimeo-embed")[0].getBoundingClientRect().top;
+    console.log(vimeoPos);
+    console.log(innerHeight);
+    if (vimeoPos < 0) {
+        navBar.style.background = "#272727";
+        navBar.style.position = "sticky";
+        navBar.getElementsByTagName("p")[0].innerHTML = "fisher.<span class=\"text-orange\">studio</span>()";
+    } else {
+        navBar.style.background = "0";
+        navBar.style.position = "static";
+        navBar.getElementsByTagName("p")[0].innerHTML = "fisher.studio";
+    }
+}
+
+// Debouncing function from html5rocks via MDN
+document.addEventListener('scroll', function(e) {
+    lastKnownScrollPosition = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            doSomething(lastKnownScrollPosition);
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+});
